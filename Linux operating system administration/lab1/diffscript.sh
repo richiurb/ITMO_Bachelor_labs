01e0eeae7545f1aad397aff20bcb1c42f3883022
Rikhard <urihard@mail.ru>
2021-09-15 19:28:51 +0300

    fix bug in diffscript in lab1 Linux
    If $2 did not exist - script have incorrect answer

--- Linux operating system administration/lab1/diffscript.sh
+++ Linux operating system administration/lab1/diffscript.sh
@@ -3,9 +3,14 @@
 # Скрипт находится в каталоге /home
 
 val1=$(diff $1 $2)
-if [[ -z $val1 ]]
+if [[ -e $2 ]]
 then
-echo "YES"
+    if [[ -z $val1 ]]
+    then
+        echo "YES"
+    else
+        echo "NO"
+    fi
 else
-echo "NO"
+    echo "NO"
 fi
